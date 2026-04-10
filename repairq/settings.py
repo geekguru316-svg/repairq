7ba843ba3047e8dd15cb1242031bd7779b2950b2
Benimaru <geekguru316@gmail.com>
2026-04-10 17:11:48 +0800

    Update settings.py

--- repairq/settings.py
+++ repairq/settings.py
@@ -59,10 +59,10 @@ TEMPLATES = [
 WSGI_APPLICATION = 'repairq.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+    )
 }
 
 # Use PostgreSQL if DATABASE_URL is set and valid (e.g. on Render)
